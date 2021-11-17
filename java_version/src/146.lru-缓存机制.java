dd8fc68e8bb35c776271488bda62604484056fdb
廖宏军 <hongjunliao@yeah.net>
2021-11-17 11:42:11 +0800

    init item

---
+++ java_version/src/146.lru-缓存机制.java
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=146 lang=java
+ *
+ * [146] LRU 缓存机制
+ */
+
+// @lc code=start
+class LRUCache {
+  int capacity;
+  LinkedHashMap<Integer, Integer> cache;
+
+  public LRUCache(int capacity) {
+    this.capacity = capacity;
+    cache = new LinkedHashMap<Integer, Integer>(capacity, 0.75f, true) {
+      @Override
+      protected boolean removeEldestEntry(Map.Entry eldest) {
+        return cache.size() > capacity;
+      }
+    };
+  }
+
+  public int get(int key) {
+    return cache.getOrDefault(key, -1);
+  }
+
+  public void put(int key, int value) {
+    cache.put(key, value);
+  }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such: LRUCache obj =
+ * new LRUCache(capacity); int param_1 = obj.get(key); obj.put(key,value);
+ */
+// @lc code=end
